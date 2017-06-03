1e01cc350e7fa9630aa93e25f1e6b397aaea3a1c
macbookpro2100 <macbookpro2100@qq.com>
2017-06-03 14:22:16 +0800

    init main

--- ink_Swift/ink_Swift/ViewController.swift
+++ ink_Swift/ink_Swift/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var index: NSInteger = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // PRAGMA: actions
+    
+    @IBAction func showBadgeHandelr(_ sender: AnyObject) {
+        index += 1
+        self.tabBarItem.badgeValue = "\(index)"
+    }
+    
+    @IBAction func hideBadgeHandler(_ sender: AnyObject) {
+        self.tabBarItem.badgeValue = nil
     }
 
 
