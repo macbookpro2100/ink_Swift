7aa54ecc706e51f355b670684bc30787983e9caa
macbookpro2100@qq.com <macbookpro2100@qq.com>
2017-11-17 09:24:54 +0800

    new Project

---
+++ Ink_Swift/Ink_Swift/Classes/Main/Contact/Sections/AddFriend/LXFAddFriendViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LXFAddFriendViewController.swift
+//  Ink_Swift
+//
+//  Created by LXFWeChat on 2016/12/28.
+//  Copyright © 2016年 LXFWeChat. All rights reserved.
+//
+//  GitHub: https://github.com/LinXunFeng
+//  简书: http://www.jianshu.com/users/31e85e7a22a2
+
+import UIKit
+
+class LXFAddFriendViewController: LXFBaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 设置标题
+        navigationItem.title = "添加好友"
+    }
+
+}
---
+++ Ink_Swift/Ink_Swift/Classes/Main/Wechat/Sections/WechatSearch/Ink_SwiftSearchController.swift
@@ -0,0 +1,26 @@
+//
+//  Ink_SwiftSearchController.swift
+//  Ink_Swift
+//
+//  Created by LXFWeChat on 2016/12/29.
+//  Copyright © 2016年 LXFWeChat. All rights reserved.
+//
+//  GitHub: https://github.com/LinXunFeng
+//  简书: http://www.jianshu.com/users/31e85e7a22a2
+
+import UIKit
+
+class Ink_SwiftSearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+        
+    }
+    
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+}
